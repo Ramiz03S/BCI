76810a7ff2ac3a1efd3308de91fb9b73516be124
Ramiz Samaien <ramizsamaien03@gmail.com>
2024-06-15 19:25:52 +0200

    Added plotting functions for both datasets to show the effects of epoching and preprocessing

--- BCI_Project.py
+++ BCI_Project.py
@@ -12,6 +12,11 @@ import scipy.signal
 from numpy import linalg
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
+import matplotlib.pyplot as plt
+from matplotlib import mlab, patches
+
+
+
 
 class MotorImageryBcic4:
     """
@@ -48,10 +53,12 @@ class MotorImageryBcic4:
         self.train_cl1_cl2 = None
         self.trials = None
         self.trials_filt = None
+        self.trials_csp = None
         self.event_codes = None
         self.w = None
         self.event_onsets = None
         self.EEG = None
+        self.channel_names = None
 
     def load_mat(self):
         """
@@ -62,6 +69,7 @@ class MotorImageryBcic4:
         # SciPy.io.loadmat does not deal well with Matlab structures, resulting in lots of
         # extra dimensions in the arrays. This makes the code a bit more cluttered
         self.sample_rate = m['nfo']['fs'][0][0][0][0]
+        self.channel_names = [s[0] for s in m['nfo']['clab'][0][0][0]]
         self.EEG = m['cnt'].T
         self.nchannels, self.nsamples = self.EEG.shape
         self.event_onsets = m['mrk'][0][0][0]
@@ -109,21 +117,28 @@ class MotorImageryBcic4:
         ntrain_l = int(self.trials_filt[self.cl2].shape[2] )
 
 
-        # Train the CSP on the training set 
+        # Train the CSP on the training set
         w = csp(self.trials_filt[self.cl1], self.trials_filt[self.cl2], self.nsamples_win)
         self.w = w
+
+        self.trials_csp = {self.cl1: np.ones_like(self.trials_filt[self.cl1]),
+                           self.cl2: np.ones_like(self.trials_filt[self.cl2])}
+
         # Apply the CSP on the training
-        self.trials_filt[self.cl1] = apply_mix(w, self.trials_filt[self.cl1])
-        self.trials_filt[self.cl2] = apply_mix(w, self.trials_filt[self.cl2])
+        self.trials_csp[self.cl1] = apply_mix(w, self.trials_filt[self.cl1])
+        self.trials_csp[self.cl2] = apply_mix(w, self.trials_filt[self.cl2])
+
+        trials_csp_cl1 = self.trials_csp[self.cl1]
+        trials_csp_cl2 = self.trials_csp[self.cl2]
 
         # Select only the first and last two components for classification
         comp = np.array([0,1,-2,-1])
-        self.trials_filt[self.cl1] = self.trials_filt[self.cl1][comp,:,:]
-        self.trials_filt[self.cl2] = self.trials_filt[self.cl2][comp,:,:]
+        trials_csp_cl1 = trials_csp_cl1[comp,:,:]
+        trials_csp_cl2 = trials_csp_cl2[comp,:,:]
 
         # Calculate the log-var
-        train_cl1 = logvar(self.trials_filt[self.cl1])
-        train_cl2 = logvar(self.trials_filt[self.cl2])
+        train_cl1 = logvar(trials_csp_cl1)
+        train_cl2 = logvar(trials_csp_cl2)
 
         train_cl1 = np.append(train_cl1.transpose(),
                             np.ones([ntrain_r,1],dtype = int),
@@ -133,6 +148,57 @@ class MotorImageryBcic4:
                             axis = 1)
 
         self.train_cl1_cl2 = np.vstack([train_cl1, train_cl2])
+
+    def plot_raw_filt_csp(self):
+
+
+        psd_r, freqs = psd(self.trials[self.cl1],self.sample_rate)
+        psd_l, freqs = psd(self.trials[self.cl2],self.sample_rate)
+        trials_PSD = {self.cl1: psd_r, self.cl2: psd_l}
+
+        plot_psd(
+            trials_PSD,
+            freqs,
+            [self.channel_names.index(ch) for ch in ['C3', 'Cz', 'C4']],
+            chan_lab=['C3', 'Cz', 'C4'],
+            maxy=500)
+
+        psd_r, freqs = psd(self.trials_filt[self.cl1],self.sample_rate)
+        psd_l, freqs = psd(self.trials_filt[self.cl2],self.sample_rate)
+        trials_filt_PSD = {self.cl1: psd_r, self.cl2: psd_l}
+
+        plot_psd(
+            trials_filt_PSD,
+            freqs,
+            [self.channel_names.index(ch) for ch in ['C3', 'Cz', 'C4']],
+            chan_lab=['C3', 'Cz', 'C4'],
+            maxy=500)
+
+        psd_r, freqs = psd(self.trials_csp[self.cl1],self.sample_rate)
+        psd_l, freqs = psd(self.trials_csp[self.cl2],self.sample_rate)
+        trials_csp_PSD = {self.cl1: psd_r, self.cl2: psd_l}
+
+        plot_psd(
+            trials_csp_PSD,
+            freqs,
+            [0,1,-2,-1],
+            chan_lab=['First Component','Second Component','Second to Last Component','Last Component'])
+
+    def plot_filt_csp_logvar(self):
+        logvar_filt_cl1 = np.ones_like(self.trials_filt[self.cl1])
+        logvar_filt_cl2 = np.ones_like(self.trials_filt[self.cl2])
+
+        logvar_filt_cl1 = logvar(self.trials_filt[self.cl1])
+        logvar_filt_cl2 = logvar(self.trials_filt[self.cl2])
+
+        logvar_csp_cl1 = np.ones_like(self.trials_csp[self.cl1])
+        logvar_csp_cl2 = np.ones_like(self.trials_csp[self.cl2])
+
+        logvar_csp_cl1 = logvar(self.trials_csp[self.cl1])
+        logvar_csp_cl2 = logvar(self.trials_csp[self.cl2])
+
+        plot_logvar(logvar_filt_cl1,logvar_filt_cl2)
+        plot_logvar(logvar_csp_cl1,logvar_csp_cl2)
 
     def eval_model(self, model):
         """
@@ -152,6 +218,65 @@ class MotorImageryBcic4:
             model.fit(x[train,:],y[train])
             score += model.score(x[test,:],y[test])
         return score*100/kf.get_n_splits()
+
+    def plot_trials(self):
+
+        fig, ax = plt.subplots()
+        fig.suptitle('EEG Recording of Channel C3 while performing Motor Imagery')
+        fig.supxlabel('time samples at 100 Hz')
+        fig.supylabel('Voltage (microV)')
+
+        try:
+            if self.EEG is None:
+                raise TypeError
+        except TypeError:
+            print('Please use load_mat() first.\n')
+        else:
+            EEG = self.EEG
+            ax.set_xlim([0, EEG.shape[1]])
+            EEG[self.channel_names.index('C3'),:] = np.divide(EEG[self.channel_names.index('C3'),:],10)
+            ax.set_ylim([np.min(EEG[self.channel_names.index('C3'),:]), np.max(EEG[self.channel_names.index('C3'),:])])
+            x = range(EEG.shape[1])
+            ax.plot(x,EEG[self.channel_names.index('C3'),:], linewidth = 0.2, color = '#7c8594')
+
+            ax.scatter(self.event_onsets[self.event_codes == 1],EEG[self.channel_names.index('C3'),self.event_onsets[self.event_codes == 1]],
+                        color = 'red',
+                        marker = 'x',
+                        linewidth = 2,
+                        zorder = 2.5)
+            ax.scatter(self.event_onsets[self.event_codes == -1],EEG[self.channel_names.index('C3'),self.event_onsets[self.event_codes == -1]],
+                        color = 'black',
+                        marker = 'x',
+                        linewidth = 2,
+                        zorder = 2.5)
+            plt.legend([None,'Right Hand','Left Hand'])
+
+            win = np.arange(int(0.5*self.sample_rate), int(2.5*self.sample_rate))
+            for cl, code in zip([self.cl1, self.cl2], np.unique(self.event_codes)):
+                cl_onsets = self.event_onsets[self.event_codes == code]
+                for _, onset in enumerate(cl_onsets):
+                    x = win+onset
+                    y = self.EEG[self.channel_names.index('C3'),x]
+                    if(cl == self.cl1):
+                        ax.plot(x,y,color = 'black',zorder = 2.5)
+                        rect = patches.Rectangle([x[0],np.max(y)],
+                                                (x[-1]-x[0]),
+                                                (np.min(y)-np.max(y)),
+                                                edgecolor = 'black',
+                                                alpha = 0.3)
+                        ax.add_patch(rect)
+                    else:
+                        ax.plot(x,y,color = 'red',zorder = 2.5)
+                        rect = patches.Rectangle([x[0],np.max(y)],
+                                                (x[-1]-x[0]),
+                                                (np.min(y)-np.max(y)),
+                                                edgecolor = 'red',
+                                                alpha = 0.3)
+                        ax.add_patch(rect)
+            plt.show(block = False)
+
+
+
 
 class MotorImageryOpenBmi:
     """
@@ -196,6 +321,7 @@ class MotorImageryOpenBmi:
         self.EEG_train = None
         self.event_codes_train = None
         self.event_onsets_train = None
+        self.trials_csp = None
 
     def load_mat(self):
         """
@@ -270,6 +396,8 @@ class MotorImageryOpenBmi:
                             'C1','C2','C3','C4','C5','C6','Cz',
                             'CP1','CP2','CP3','CP4','CP5','CP6','CPz']
 
+        self.channel_names = required_channels
+
         EEG_train_20_channels = np.zeros((20,self.EEG_train.shape[1]))
 
         for i, _ in enumerate(required_channels):
@@ -277,10 +405,10 @@ class MotorImageryOpenBmi:
             EEG_train_20_channels[i,:] = self.EEG_train[index,:]
 
         a, b = scipy.signal.butter(5,
-                                   [8,30],
-                                   'bandpass',
-                                   analog = False,
-                                   fs=1000)
+                                [8,30],
+                                'bandpass',
+                                analog = False,
+                                fs=1000)
 
         EEG_train_20_filt = np.zeros_like(EEG_train_20_channels)
         for i in range(20):
@@ -329,17 +457,24 @@ class MotorImageryOpenBmi:
         # Apply the CSP on the training set, gives the spatial filtered signal
         # where the first and last rows contain the filters that maximize the
         # variance between the 2 classes
-        self.trials[self.cl1] = apply_mix(w, self.trials[self.cl1])
-        self.trials[self.cl2] = apply_mix(w, self.trials[self.cl2])
+
+        self.trials_csp = {self.cl1: np.ones_like(self.trials[self.cl1]),
+                           self.cl2: np.ones_like(self.trials[self.cl2])}
+
+        self.trials_csp[self.cl1] = apply_mix(w, self.trials[self.cl1])
+        self.trials_csp[self.cl2] = apply_mix(w, self.trials[self.cl2])
+
+        trials_csp_cl1 = self.trials_csp[self.cl1]
+        trials_csp_cl2 = self.trials_csp[self.cl2]
 
         # Select only the first 2 and last 2 components for classification
         comp = np.array([0,1,-2,-1])
-        self.trials[self.cl1] = self.trials[self.cl1][comp,:,:]
-        self.trials[self.cl2] = self.trials[self.cl2][comp,:,:]
+        trials_csp_cl1 = trials_csp_cl1[comp,:,:]
+        trials_csp_cl2 = trials_csp_cl2[comp,:,:]
 
         # Calculate the log-var
-        train_cl1 = logvar(self.trials[self.cl1])
-        train_cl2 = logvar(self.trials[self.cl2])
+        train_cl1 = logvar(trials_csp_cl1)
+        train_cl2 = logvar(trials_csp_cl2)
 
         train_cl1 = np.append(train_cl1.transpose(),
                               np.ones([ntrain_r,1],dtype = int),
@@ -350,6 +485,43 @@ class MotorImageryOpenBmi:
         # train_cl1_cl2: 2d array of shape (trials x (features + 1))
         # 4 features for each trial along with label of trial
         self.train_cl1_cl2 = np.vstack([train_cl1, train_cl2])
+
+    def plot_filt_csp(self):
+        psd_r, freqs = psd(self.trials[self.cl1],self.sample_rate)
+        psd_l, freqs = psd(self.trials[self.cl2],self.sample_rate)
+        trials_PSD = {self.cl1: psd_r, self.cl2: psd_l}
+
+        plot_psd(
+            trials_PSD,
+            freqs,
+            [self.channel_names.index(ch) for ch in ['C3', 'Cz', 'C4']],
+            chan_lab=['C3', 'Cz', 'C4'])
+
+        psd_r, freqs = psd(self.trials_csp[self.cl1],self.sample_rate)
+        psd_l, freqs = psd(self.trials_csp[self.cl2],self.sample_rate)
+        trials_csp_PSD = {self.cl1: psd_r, self.cl2: psd_l}
+
+        plot_psd(
+            trials_csp_PSD,
+            freqs,
+            [0,1,-2,-1],
+            chan_lab=['First Component','Second Component','Second to Last Component','Last Component'])
+
+    def plot_logvar_filt_csp(self):
+        logvar_filt_cl1 = np.ones_like(self.trials[self.cl1])
+        logvar_filt_cl2 = np.ones_like(self.trials[self.cl2])
+
+        logvar_filt_cl1 = logvar(self.trials[self.cl1])
+        logvar_filt_cl2 = logvar(self.trials[self.cl2])
+
+        logvar_csp_cl1 = np.ones_like(self.trials_csp[self.cl1])
+        logvar_csp_cl2 = np.ones_like(self.trials_csp[self.cl2])
+
+        logvar_csp_cl1 = logvar(self.trials_csp[self.cl1])
+        logvar_csp_cl2 = logvar(self.trials_csp[self.cl2])
+
+        plot_logvar(logvar_filt_cl1,logvar_filt_cl2)
+        plot_logvar(logvar_csp_cl1,logvar_csp_cl2)
 
     def eval_model(self, model):
         """
@@ -475,6 +647,128 @@ def apply_mix(w, trials):
         trials_csp[:,:,i] = w.T.dot(trials[:,:,i])
     return trials_csp
 
+def psd(trials,sample_rate):
+    '''
+    Calculates for each trial the Power Spectral Density (PSD).
+
+    Parameters
+    ----------
+    trials : 3d-array (channels x samples x trials)
+        The EEG signal
+    sample_rate : float
+        Sample rate of the signal (in Hz)
+
+    Returns
+    -------
+    trial_PSD : 3d-array (channels x PSD x trials)
+        the PSD for each trial.
+    freqs : list of floats
+        The frequencies for which the PSD was computed (useful for plotting later)
+    '''
+
+    nchannels = trials.shape[0]
+    nsamples = trials.shape[1]
+    ntrials = trials.shape[2]
+
+    npoints = (nsamples / 2) + 1
+    trials_PSD = np.zeros((nchannels, int(npoints), ntrials))
+
+    # Iterate over trials and channels
+    for trial in range(ntrials):
+        for ch in range(nchannels):
+            # Calculate the PSD
+            (PSD, freqs) = mlab.psd(trials[ch,:,trial], NFFT=int(nsamples), Fs=sample_rate)
+            trials_PSD[ch, :, trial] = PSD.ravel()
+
+    return trials_PSD, freqs
+
+def plot_psd(trials_PSD, freqs, chan_ind, chan_lab=None, maxy=None):
+    '''
+    Plots PSD data calculated with psd().
+
+    Parameters
+    ----------
+    trials : 3d-array
+        The PSD data, as returned by psd()
+    freqs : list of floats
+        The frequencies for which the PSD is defined, as returned by psd()
+    chan_ind : list of integers
+        The indices of the channels to plot
+    chan_lab : list of strings
+        (optional) List of names for each channel
+    maxy : float
+        (optional) Limit the y-axis to this value
+    '''
+    plt.figure(figsize=(12,5))
+
+    nchans = len(chan_ind)
+
+    # Maximum of 3 plots per row
+    if nchans == 4:
+        nrows = 2
+        ncols = 2
+    else:
+        nrows = int(np.ceil(nchans / 3))
+        ncols = min(4, nchans)
+
+    # Enumerate over the channels
+    for i,ch in enumerate(chan_ind):
+        # Figure out which subplot to draw to
+        plt.subplot(nrows,ncols,i+1)
+
+        # Plot the PSD for each class
+        for cl in trials_PSD.keys():
+            plt.plot(freqs, np.mean(trials_PSD[cl][ch,:,:], axis=1), label=cl)
+
+        # All plot decoration below...
+
+        plt.xlim(1,35)
+
+        if maxy != None:
+            plt.ylim(0,maxy)
+
+        plt.grid()
+
+        plt.xlabel('Frequency (Hz)')
+
+        if chan_lab == None:
+            plt.title('Channel %d' % (ch+1))
+        else:
+            plt.title(chan_lab[i])
+
+        plt.legend()
+
+    plt.tight_layout()
+    plt.show(block = False)
+
+def plot_logvar(trials_cl1, trials_cl2):
+    '''
+    Plots the log-var of each channel/component.
+    arguments:
+        trials - Dictionary containing the trials (log-vars x trials) for 2 classes.
+    '''
+    plt.figure(figsize=(12,5))
+
+    nchannels = trials_cl1.shape[0]
+
+    x0 = np.arange(nchannels)
+    x1 = np.arange(nchannels) + 0.4
+
+    y0 = np.mean(trials_cl1, axis=1)
+    y1 = np.mean(trials_cl2, axis=1)
+
+    plt.bar(x0, y0, width=0.5, color='b')
+    plt.bar(x1, y1, width=0.4, color='r')
+
+    plt.xlim(-0.5, nchannels+0.5)
+
+    plt.gca().yaxis.grid(True)
+    plt.title('log-var of each channel/component')
+    plt.xlabel('channels/components')
+    plt.ylabel('log-var')
+    plt.legend(['right','left'])
+    plt.show(block = False)
+
 if __name__ == "__main__":
     while True:
         try:
@@ -497,6 +791,10 @@ if __name__ == "__main__":
                     d.feature_extract_trials()
                     SCORE = d.eval_model(model)
                     print(f'Accuracy of prediction for data/BCICIV_calib_ds1{letter}.mat is {round(SCORE,2)}')
+                    d.plot_trials()
+                    d.plot_raw_filt_csp()
+                    d.plot_filt_csp_logvar()
+                    plt.show()
                 break
             except FileNotFoundError:
                 print('Requested file(s) not found\n')
@@ -513,6 +811,9 @@ if __name__ == "__main__":
                     d.feature_extract_trials()
                     SCORE = d.eval_model(model)
                     print(f'Accuracy of prediction for data/sess01_subj{num}_EEG_MI.mat is {round(SCORE,2)}')
+                    d.plot_filt_csp()
+                    d.plot_logvar_filt_csp()
+                    plt.show()
                 break
             except FileNotFoundError:
                 print('Requested file(s) not found\n')
